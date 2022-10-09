16ee33002fc6f12993c9066a378ba7fcb60678bf
dragos <dragos.duma@gmail.com>
2022-10-09 15:38:40 +0300

    add mave/spring project template

---
+++ sports-network-server/src/main/java/com/dragos/sportsnetworkserver/SportsNetworkServerApplication.java
@@ -0,0 +1,13 @@
+package com.dragos.sportsnetworkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsNetworkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsNetworkServerApplication.class, args);
+	}
+
+}
---
+++ sports-network-server/src/test/java/com/dragos/sportsnetworkserver/SportsNetworkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragos.sportsnetworkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsNetworkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
