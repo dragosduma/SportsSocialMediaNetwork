7f9ca245f8d82d83f3d4697a1a4e0ce06cc072f5
dragos <dragos.duma@gmail.com>
2022-10-25 23:14:29 +0300

    new dependencies and plugin configuration

--- sports-network-server/src/main/java/com/dragos/sportsnetworkserver/SportsNetworkServerApplication.java
+++ sports-network-server/src/main/java/com/dragos/sportsnetworkserver/SportsNetworkServerApplication.java
@@ -1,10 +1,45 @@
 package com.dragos.sportsnetworkserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.Comparator;
 
 @SpringBootApplication
+@Slf4j
 public class SportsNetworkServerApplication {
+	@Component
+	public static final class EndpointsListener implements ApplicationListener<ContextRefreshedEvent> {
+
+		@Override
+		public void onApplicationEvent(ContextRefreshedEvent event) {
+
+			log.info("Listing all registered endpoints:");
+
+			ApplicationContext applicationContext = event.getApplicationContext();
+
+			applicationContext.getBean(RequestMappingHandlerMapping.class)
+					.getHandlerMethods()
+					.entrySet()
+					.stream()
+					.filter(e -> e.getKey().getPathPatternsCondition() != null)
+					.sorted(Comparator.comparing(e -> e.getKey().getPathPatternsCondition().getPatterns().iterator().next()))
+					.forEach(
+							entry -> {
+								var iterator = entry.getKey().getMethodsCondition().getMethods().iterator();
+								log.info("{}{} -> {}()", entry.getKey().getPathPatternsCondition().getPatterns(),
+										iterator.hasNext() ? iterator.next().name() : "",
+										entry.getValue().getMethod().getName());
+							}
+					);
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportsNetworkServerApplication.class, args);
---
+++ sports-network-server/src/main/java/controllers/EventsController.java
@@ -0,0 +1,37 @@
+package controllers;
+import com.dragos.sportsnetworkserver.api.EventApi;
+import com.dragos.sportsnetworkserver.model.Event;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EventsController implements EventApi {
+    @Override
+    public ResponseEntity<Void> createEvent(Event event) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteEvent(String eventId) {
+        return null;
+
+    }
+
+    @Override
+    public ResponseEntity<Event> getEvent(String eventId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Event>> getEvents() {
+        System.out.println("got events");
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> updateEvent(String eventId, Event event) {
+        return null;
+    }
+}
