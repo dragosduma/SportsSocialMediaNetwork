f2c8edca6d03864cdc2af7e17db11741c91d914d
dragos <dragos.duma@gmail.com>
2022-11-11 13:01:33 +0200

    Added tailwind forms, hero icons and search bar

--- sports-network-client/sportssocialmedianetwork/components/Header.js
+++ sports-network-client/sportssocialmedianetwork/components/Header.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/outline';
+
 export default function Header() {
     return (
-        <div>
 
-            <div className='flex items-center justify-between max-w-6xl'>
-                <div className='h-60 w-60 relative hidden lg:inline-grid'>
-                    <Image src="/Sports social media network.png" layout='fill' className='object-contain' />
-                </div>
+
+        <div className='flex items-center justify-between max-w-6xl'>
+            <div className='h-60 w-60 relative hidden lg:inline-grid'>
+                <Image
+                    src="/Sports social media network.png"
+                    layout='fill'
+                    lassName='object-contain' />
             </div>
             <div className='h-40 w-40 relative lg:hidden'>
-                <Image src="/Sports social media network small.png" layout='fill' className='object-contain' />
+                <Image
+                    src="/Sports social media network small.png"
+                    layout='fill'
+                    className='object-contain' />
             </div>
 
+            <div className="relative mt-1">
+                <div className="absolute top-2 left-2">
+                    <SearchIcon className="h-5 text-gray-500" />
+                </div>
+                <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
+            </div>
         </div>
+
+
+
     )
 }
