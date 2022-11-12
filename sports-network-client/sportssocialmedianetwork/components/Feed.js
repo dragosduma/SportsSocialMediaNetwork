2508f01892a74becdeeca84834acdc46679f533f
dragos <dragos.duma@gmail.com>
2022-11-12 21:17:34 +0200

    Create suggestions component

--- sports-network-client/sportssocialmedianetwork/components/Feed.js
+++ sports-network-client/sportssocialmedianetwork/components/Feed.js
@@ -1,25 +1,27 @@
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
+import Suggestions from "./Suggestions";
 
 export default function Feed() {
-  return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
-        <section className="md:col-span-2">
-            {/*stories*/}
-            <Stories/>
+    return (
+        <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+            <section className="md:col-span-2">
+                {/*stories*/}
+                <Stories />
 
-            {/*posts*/}
-            <Posts/>
-        </section>
-        
-        <section className="hidden md:inline-grid md:col-span-1">
-            <div className="fixed w-[380px]">
-            {/*mini profile*/}
-            <MiniProfile/>
-            {/*suggestions*/}
-            </div>
-        </section>
-    </main>
-  );
+                {/*posts*/}
+                <Posts />
+            </section>
+
+            <section className="hidden md:inline-grid md:col-span-1">
+                <div className="fixed w-[380px]">
+                    {/*mini profile*/}
+                    <MiniProfile />
+                    {/*suggestions*/}
+                    <Suggestions />
+                </div>
+            </section>
+        </main>
+    );
 }
