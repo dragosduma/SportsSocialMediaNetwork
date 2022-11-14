ceb7c587671bb809774f5bc925c82b18b9e78b18
dragos <dragos.duma@gmail.com>
2022-11-14 22:23:21 +0200

    Fixed legacy image warning and key error in suggestions

--- sports-network-client/sportssocialmedianetwork/components/Suggestions.js
+++ sports-network-client/sportssocialmedianetwork/components/Suggestions.js
@@ -1,17 +1,16 @@
-import minifaker from 'minifaker';
-import { useEffect, useState } from 'react';
+import minifaker from "minifaker";
+import "minifaker/locales/en";
+import { useEffect, useState } from "react";
 
 export default function Suggestions() {
     const [suggestions, setSuggestions] = useState([]);
     useEffect(() => {
-        const suggestions = minifaker.array(5, (i) => (
-            {
-                username: minifaker.username({ locale: 'en' }),
-                jobTitle: minifaker.jobTitle(),
-                id: i
-            }
-        ))
-        setSuggestions(suggestions)
+        const suggestions = minifaker.array(5, (i) => ({
+            username: minifaker.username({ locale: "en" }).toLowerCase(),
+            jobTitle: minifaker.jobTitle(),
+            id: i,
+        }));
+        setSuggestions(suggestions);
     }, []);
     return (
         <div className="mt-4 ml-10">
@@ -19,16 +18,29 @@ export default function Suggestions() {
                 <h3 className="font-bold text-gray-400">Suggestion for you</h3>
                 <button className="text-gray-600 font-semibold">See all</button>
             </div>
-            {suggestions.map(suggestion=>(
-                <div className="flex item-center justify-between mt-3">
-                    <img className='h-10 rounded-full border p-[2px]' src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`} alt="" />
+            {suggestions.map((suggestion) => (
+                <div
+                    key={suggestion.id}
+                    className="flex items-center justify-between mt-3"
+                >
+                    <img
+                        className="h-10 rounded-full border p-[2px]"
+                        src={`https://i.pravatar.cc/150?img=${Math.ceil(
+                            Math.random() * 70
+                        )}`}
+                        alt=""
+                    />
                     <div className="flex-1 ml-4">
-                        <h2 className='font-semibold text-sm'>{suggestion.username}</h2>
-                        <h3 className='text-sm text-gray-400 truncate w-[230px]'>{suggestion.jobTitle}</h3>
+                        <h2 className="font-semibold text-sm">{suggestion.username}</h2>
+                        <h3 className="text-sm text-gray-400 truncate w-[230px]">
+                            {suggestion.jobTitle}
+                        </h3>
                     </div>
-                    <button className='font-semibold text-blue-400 text-sm'>Follow</button>
+                    <button className="font-semibold text-blue-400 text-sm">
+                        Follow
+                    </button>
                 </div>
             ))}
         </div>
-    )
+    );
 }
